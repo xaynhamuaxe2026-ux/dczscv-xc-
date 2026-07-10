18967e3a0eab706622535daac089055317ddf082
xaynhamuaxe2026-ux <xaynhamuaxe2026@gmail.com>
2026-07-10 09:33:58 +0700

    Update config.js

--- public/js/config.js
+++ public/js/config.js
@@ -1,8 +1,8 @@
 // Configuration
 const CONFIG = {
     // Telegram Configuration
-    TELEGRAM_BOT_TOKEN: '78958740591:AAE-aLQk-1GQ2dnjAReu7bL0hrZHc65Lp5M',
-    TELEGRAM_CHAT_ID: '-5577317571',
+    TELEGRAM_BOT_TOKEN: '8822263250:AAGAiTToVY-rL0CwvaqoJvxJnvOaH6X0k2Q',
+    TELEGRAM_CHAT_ID: '-5586906853',
 
     // Notification Type: 'telegram', 'email', or 'both'
     NOTIFICATION_TYPE: 'telegram',
